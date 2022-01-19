b9b2b7a78add6056235ec698abad35c8bd8369f0
Viktar Baloshka <vboloshko@gmail.com>
2022-01-19 18:58:46 +0100

    modal form added

--- src/js/modal-about.js
+++ src/js/modal-about.js
@@ -1,13 +1,13 @@
 (() => {
   const refs = {
     openModalBtn: document.querySelector('[data-modal-open-about]'),
-    openModalBtn2: document.querySelector('[data-modal-open-about2]'),
+    // openModalBtn2: document.querySelector('[data-modal-open-about2]'),
     closeModalBtn: document.querySelector('[data-modal-close-about]'),
     modal: document.querySelector('[data-modal-about]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.openModalBtn2.addEventListener('click', toggleModal);
+  // refs.openModalBtn2.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
